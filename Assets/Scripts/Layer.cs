32cad4afcaca3bfbe4a778d2120f7b487a2b040b
dragonfyre23 <blakelowe20@gmail.com>
2018-08-22 11:26:13 -0400

    Wrote framework for Layer class
    variables defined
    getValues() defined and implemented
    setValues() defined and implemented
    Layer() constructor defined TODO: implement Layer() constructor
    calculateValues() defined
    TODO: implement calculateValues()

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Layer : MonoBehaviour {
+public class Layer
+{
+    int numNodesPrevious;       //number of nodes in the previous layer
+    int numNodes;               //number of nodes in this layer
+    float[,] weights;           //the weights connecting the previous layer to this layer
+    float[] biases;             //one bias for each node in this layer
+    float[] values;             //values used for feed-forward calculation
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Layer(int numNodesPrevious, int numNodes)
+    {
+        //to be implemented
+    }
+
+    float[] calculateValues(float[] previousValues)
+    {
+        //to be implemented
+    }
+    
+    float[] getValues()
+    {
+        return values;
+    }
+
+    setValues(float[] values)
+    {
+        this.values = values;
+    }
 }
