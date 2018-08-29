77ff83d7acd01e067a18dec5395e8df5841dd681
dragonfyre23 <blakelowe20@gmail.com>
2018-08-29 12:32:43 -0400

    implemented constructors NeuralNet.cs
    added dependency on system
    defined field rand with get() and set()
    implemented NeuralNet()
    defined and implemented overloaded NerualNet()

--- Assets/Scripts/NeuralNet.cs
+++ Assets/Scripts/NeuralNet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 public class NeuralNet
@@ -6,15 +7,65 @@ public class NeuralNet
     readonly int numInputs;          //number of nodes in the input layer of the net
     readonly int numOutputs;         //number of nodes in the output layer of the net
     readonly int numHiddenLayers;    //the number of hidden layers
-    readonly int hiddenLayerSize;    //the number of nodes in each hidden layer //DOES NOT SUPPORT VARYING HIDDEN LAYER SIZES//
-    private readonly Layer[] hiddenLayers;
-    
-    public NeuralNet(int numInputs, int numOutputs, int numHiddenLayers, int hiddenLayerSize)
+    readonly int hiddenLayerSize;    //the number of nodes in each hidden layer //DOES NOT SUPPORT VARYING HIDDEN LAYER SIZES// although I don't see why not(8/29)
+    private readonly Layer[] layers; //including hidden layers and output layers but not input layer. Logic works by attaching a set of weights to before a layer.
+
+    Random rand;
+
+    public Random Rand
     {
-        //TODO
+        get
+        {
+            return rand;
+        }
+
+        set
+        {
+            rand = value;
+        }
     }
 
-    void FeedForward(Layer input)
+    public NeuralNet(int numInputs, int numOutputs, int numHiddenLayers, int hiddenLayerSize)
+    {
+        this.numInputs = numInputs;
+        this.numOutputs = numOutputs;
+        this.numHiddenLayers = numHiddenLayers;
+        this.hiddenLayerSize = hiddenLayerSize;
+
+        //instantiate layers
+        layers = new Layer[numHiddenLayers + 1];    //+1 for output layer
+        //assign first term (different because must fit into the input layer)
+        layers[0] = new Layer(numInputs, hiddenLayerSize);
+        for (int i = 1; i < layers.Length - 1; i++)
+        {
+            layers[i] = new Layer(hiddenLayerSize, hiddenLayerSize);
+        }
+        //assign last term (output Layer)
+        layers[layers.Length - 1] = new Layer(hiddenLayerSize, numOutputs);
+        
+    }
+
+    public NeuralNet(int numInputs, int numOutputs, int numHiddenLayers, int hiddenLayerSize, Random rand)//constructor to pass the random object to layers to enable reproduction of trials
+    {
+        this.numInputs = numInputs;
+        this.numOutputs = numOutputs;
+        this.numHiddenLayers = numHiddenLayers;
+        this.hiddenLayerSize = hiddenLayerSize;
+        this.rand = rand;
+
+        //instantiate layers
+        layers = new Layer[numHiddenLayers + 1];    //+1 for output layer
+        //assign first term (different because must fit into the input layer)
+        layers[0] = new Layer(numInputs, hiddenLayerSize, rand);
+        for (int i = 1; i < layers.Length - 1; i++)
+        {
+            layers[i] = new Layer(hiddenLayerSize, hiddenLayerSize, rand);
+        }
+        //assign last term (output Layer)
+        layers[layers.Length - 1] = new Layer(hiddenLayerSize, numOutputs, rand);
+    }
+
+    void FeedForward(double[] inputValues) //inputValues.Length must equal numInputs
     {
         //TODO
     }
