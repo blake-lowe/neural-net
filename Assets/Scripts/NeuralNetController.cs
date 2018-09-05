1467e17f41c5620131539071bb12f2c518e3877a
dragonfyre23 <blakelowe20@gmail.com>
2018-09-05 11:36:00 -0400

    started visual implementation of net in NeuralNetController.cs
    instatiates a number of cubes which scales with neural net dimensions

--- Assets/Scripts/NeuralNetController.cs
+++ Assets/Scripts/NeuralNetController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class NeuralNetController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private readonly NeuralNet net = new NeuralNet(1,1,2,3);
+    [SerializeField]
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < net.NumHiddenLayers; i++)
+        {
+            for (int j = 0; j < net.HiddenLayerSize; j++)
+            {
+                Instantiate(prefab, new Vector3(2*i, 0, 2*j), Quaternion.identity);
+            }
+        }
+        double[] outputs = net.FeedForward(new double[] { 0.5 });
+        Debug.Log(outputs[0]);
+        Debug.Log(net.Layers[0].Weights[0, 0]);
+        Debug.Log(net.Layers[0].Weights[0, 1]);
+        Debug.Log(net.Layers[1].Weights[0, 0]);
+        Debug.Log(net.Layers[1].Weights[0, 1]);
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
