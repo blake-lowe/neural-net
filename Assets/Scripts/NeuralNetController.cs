4f5c31da64ec989fc4d02927e1bf65852a074fd3
dragonfyre23 <blakelowe20@gmail.com>
2018-09-06 13:06:50 -0400

    basic visualization NeuralNetController.cs
    defined and implemented UpdateValues()
    implemented Start()

--- Assets/Scripts/NeuralNetController.cs
+++ Assets/Scripts/NeuralNetController.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class NeuralNetController : MonoBehaviour {
 
-    private readonly NeuralNet net = new NeuralNet(1,1,2,3);
-    [SerializeField]
-    public GameObject prefab;
+    private readonly NeuralNet net = new NeuralNet(1,3,5,5);//not serialized
+    
+    public GameObject prefab;//serialized
+    public GameObject textPrefab;//serialized
 
+    private GameObject[,] nodes;
+
+    private void Awake()
+    {
+        if (net.HiddenLayerSize >= net.NumOutputs)//will run most of the time
+        {
+            nodes = new GameObject[net.NumHiddenLayers + 1, net.HiddenLayerSize];
+        }
+        else//will only run if there are more outputs than nodes per hidden layer
+        {
+            nodes = new GameObject[net.NumHiddenLayers + 1, net.NumOutputs];
+        }
+        
+    }
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < net.NumHiddenLayers; i++)
+
+        double[] outputs = net.FeedForward(new double[] { 0.5 });
+
+        for (int i = 0; i < net.NumHiddenLayers; i++)//hidden layers
         {
             for (int j = 0; j < net.HiddenLayerSize; j++)
             {
-                Instantiate(prefab, new Vector3(2*i, 0, 2*j), Quaternion.identity);
+                GameObject temp = Instantiate(prefab, new Vector3(2*i, 0, 2*j), Quaternion.identity);
+                GameObject text = Instantiate(textPrefab, temp.transform);
+                nodes[i, j] = text;
             }
         }
-        double[] outputs = net.FeedForward(new double[] { 0.5 });
+        for (int j = 0; j < net.NumOutputs; j++)//output layer
+        {
+            GameObject temp = Instantiate(prefab, new Vector3(2 * net.NumHiddenLayers, 0, 2 * j), Quaternion.identity);
+            GameObject text = Instantiate(textPrefab, temp.transform);
+            nodes[net.NumHiddenLayers, j] = text;
+        }
+
+
+        UpdateValues();
         Debug.Log(outputs[0]);
-        Debug.Log(net.Layers[0].Weights[0, 0]);
-        Debug.Log(net.Layers[0].Weights[0, 1]);
-        Debug.Log(net.Layers[1].Weights[0, 0]);
-        Debug.Log(net.Layers[1].Weights[0, 1]);
     }
 
 	// Update is called once per frame
@@ -31,4 +55,19 @@ public class NeuralNetController : MonoBehaviour {
     {
 		
 	}
+
+    void UpdateValues()
+    {
+        for (int i = 0; i < net.NumHiddenLayers; i++)
+        {
+            for (int j = 0; j < net.HiddenLayerSize; j++)
+            {
+                nodes[i, j].GetComponent<TMPro.TextMeshPro>().text = net.Layers[i].Values[j].ToString();
+            }
+        }
+        for (int j = 0; j < net.NumOutputs; j++)//output layer
+        {
+            nodes[net.NumHiddenLayers, j].GetComponent<TMPro.TextMeshPro>().text = net.Layers[net.NumHiddenLayers].Values[j].ToString();
+        }
+    }
 }
