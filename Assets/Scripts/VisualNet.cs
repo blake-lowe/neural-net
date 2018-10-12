3104b5073f5b32093650b1bbd507f1c8b5f73a26
dragonfyre23 <blakelowe20@gmail.com>
2018-10-12 09:49:07 -0400

    Visualization almost complete

--- Assets/Scripts/VisualNet.cs
+++ Assets/Scripts/VisualNet.cs
@@ -8,6 +8,8 @@ public class VisualNet : MonoBehaviour {
     public float layerSeparation;
     public float nodeSeparation;
 
+    public GameObject nodeParent;//should be empty gameObject
+    public GameObject weightParent;//should be empty gameObject
     public GameObject nodePrefab;
     public GameObject weightPrefab;
 
@@ -16,62 +18,71 @@ public class VisualNet : MonoBehaviour {
 
     [System.NonSerialized]
     public NeuralNet net;
+    private bool isVNetInitialized = false;
     private GameObject[,] nodes;
     private GameObject[,,] weights;
-	// Use this for initialization
-	
-	// Update is called once per frame
-	void Update () {//set colors
-        if (net != null)
+    // Use this for initialization
+    public void Initialize()
+    {
+        //instantiate array
+        nodes = new GameObject[net.NumHiddenLayers + 1, net.HiddenLayerSize];
+
+        //fill array and set positions of prefabs
+        for (int i = 0; i < nodes.GetLength(0); i++)
+        {
+            for (int j = 0; j < nodes.GetLength(1); j++)
+            {
+                Vector3 position = new Vector3(origin.x + layerSeparation * (i + 1), origin.y + nodeSeparation * j, origin.z);
+                nodes[i, j] = Instantiate(nodePrefab, position, Quaternion.identity, nodeParent.GetComponent<Transform>());
+            }
+        }
+        //instantiate array
+        weights = new GameObject[net.NumHiddenLayers + 1, net.HiddenLayerSize, net.HiddenLayerSize + 1];
+        //fill array and set position vectors of lineRenderers
+        for (int i = 0; i < weights.GetLength(0); i++)
+        {
+            for (int j = 0; j < weights.GetLength(1); j++)
+            {
+                for (int k = 0; k < weights.GetLength(2); k++)
+                {
+                    Vector3 position0 = new Vector3(origin.x + layerSeparation * (i + 1), origin.y + nodeSeparation * j, origin.z);//node
+                    Vector3 position1 = new Vector3(origin.x + layerSeparation * i, origin.y + nodeSeparation * k, origin.z);//node previous
+                    weights[i, j, k] = Instantiate(weightPrefab, weightParent.GetComponent<Transform>());
+                    LineRenderer lr = weights[i, j, k].GetComponent<LineRenderer>();
+                    lr.SetPosition(0, position0);
+                    lr.SetPosition(1, position1);
+                }
+            }
+        }
+        isVNetInitialized = true;
+    }
+    // Update is called once per frame
+    void Update () {//set colors
+        if (net != null && isVNetInitialized)
         {
             for (int i = 0; i < nodes.GetLength(0); i++)
             {
                 for (int j = 0; j < nodes.GetLength(1); j++)
                 {
-                    //todo
+                    double value = net.Layers[i].Values[j];//grab value//error here due to output layer having fewer values (to fix. change all loops to skip output layer and don't even fill the array with prefabs in the first place
+                    Color colorValue = Helper.InterpolateColor(Color0, Color1, value);//calculate color
+                    nodes[i, j].GetComponent<Renderer>().material.SetColor("_Color", colorValue);//set color of material
                 }
             }
             for (int i = 0; i < weights.GetLength(0); i++)
             {
                 for (int j = 0; j < weights.GetLength(1); j++)
                 {
-                    for (int k = 0; k < weights.Length; k++)
+                    for (int k = 0; k < weights.GetLength(2); k++)
                     {
-                        //todo
+                        double weight = net.Layers[i].Weights[j, k];//grab weight
+                        Color colorWeight = Helper.InterpolateColor(Color0, Color1, weight);//calculate color
+                        weights[i, j, k].GetComponent<LineRenderer>().material.SetColor("_Color", colorWeight);
                     }
                 }
             }
         }
 	}
-    public void Initiate()
-    {
-        nodes = new GameObject[net.NumHiddenLayers + 1, net.HiddenLayerSize];
-
-        for (int i = 0; i < nodes.GetLength(0); i++)
-        {
-            for (int j = 0; j < nodes.GetLength(1); j++)
-            {
-                Vector3 position = new Vector3(origin.x + layerSeparation * (i+1), origin.y + nodeSeparation * j, origin.z);
-                nodes[i, j] = Instantiate(nodePrefab)//todo
-            }
-        }
-
-        weights = new GameObject[net.NumHiddenLayers + 1, net.HiddenLayerSize, net.HiddenLayerSize + 1];
-
-        for (int i = 0; i < weights.GetLength(0); i++)
-        {
-            for (int j = 0; j < weights.GetLength(1); j++)
-            {
-                for (int k = 0; k < weights.Length; k++)
-                {
-                    Vector3 position1 = new Vector3(origin.x + layerSeparation * (i + 1), origin.y + nodeSeparation * j, origin.z);//node
-                    Vector3 position2 = new Vector3(origin.x + layerSeparation * i, origin.y + nodeSeparation * k, origin.z);//node previous
-                    weights[i, j, k] = Instantiate(weightPrefab);
-                    //todo //add to array
-                }
-            }
-        }
-    }
 }
 
 static class Helper
--- Assets/Scripts/VisualNetController.cs
+++ Assets/Scripts/VisualNetController.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VisualNetController : MonoBehaviour {
+public class VisualNetController : MonoBehaviour
+{
 
     public VisualNet vNet;
-	// Use this for initialization
-	void Start () {
-        vNet.net = new NeuralNet(2, 2, 5, 5, 0.1);
+    // Use this for initialization
+    void Start()
+    {
+        vNet.net = new NeuralNet(1, 1, 4, 3, 0.1);
+        vNet.net.FeedForward(new double[] { 0.1 });
         vNet.Initialize();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
