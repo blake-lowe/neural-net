abd8e249926808c5dc228001bc0348e53be28da3
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2018-11-13 11:34:37 -0500

    further implementation of genetic algorithm

--- Assets/Scripts/GeneticAlgorithm.cs
+++ Assets/Scripts/GeneticAlgorithm.cs
@@ -1,16 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneticAlgorithm : MonoBehaviour {
+public class GeneticAlgorithm {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int populationSize;
+    public int numParents;
+    public float environmentalPressure;//from 0 to 1. 1 is no survivors
+    public int numToKill;
+
+    private GeneticIndividual[] individuals;
+    // Use this for initialization
+    public GeneticAlgorithm(int populationSize, int numParents, float environmentalPressure)
+    {
+        this.populationSize = populationSize;
+        this.numParents = numParents;
+        this.environmentalPressure = environmentalPressure;
+        numToKill = (int)(environmentalPressure * populationSize);
+
+
+        individuals = new GeneticIndividual[populationSize];
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        for (int i = 0; i < individuals.Length; i++)
+        {
+            individuals[i] = individuals[i].randomize();
+        }
+    }
+
+    private void FitnessSort()
+    {
+        Array.Sort(individuals);//will sort individuals based on fitness because GeneticIndividual and classes which implement GeneticIndividual impelements CompareTo and IComparable
+    }
+
+    private GeneticIndividual TrainGeneration(int numGenerations)
+    {
+        for (int i = 0; i < numGenerations; i++)
+        {
+            //one iteration
+        }
+        return individuals[0];//the highest fitness
+    }
+
+    private GeneticIndividual TrainFitness(double targetFitness)
+    {
+        return individuals[0];//the highest fitness
+    }
 }
--- Assets/Scripts/GeneticIndividual.cs
+++ Assets/Scripts/GeneticIndividual.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface GeneticIndividual<T>
+public interface GeneticIndividual : IComparable//if IComparable isn't availible then make sure "using System;"
 {
     double fitness();//return how fit the individual is
 
-    IList<T> reproduce(IList<T> parents, int crossoverPoints, int numChildren);//take an array of individuals and return an array of children
+    GeneticIndividual randomize();
 
-    T mutate();//change a bit of the genes
+    GeneticIndividual[] reproduce(GeneticIndividual[] parents, int crossoverPoints, int numChildren);//take an array of individuals and return an array of children
+
+    GeneticIndividual mutate();//change a bit of the genes
+
+    int CompareTo(GeneticIndividual individual); //for help implementing: https://docs.microsoft.com/en-us/dotnet/api/system.icomparable-1.compareto?view=netframework-4.7.2
 }
