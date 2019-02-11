b9bcd97dad64b0f56fa4496dcf5a8f74fed9b2e6
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2019-02-11 14:11:02 -0500

    hardcoding testing

--- Assets/Scripts/HardCodeNNTest.cs
+++ Assets/Scripts/HardCodeNNTest.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class HardCodeNNTest : MonoBehaviour {
 
-	// Use this for initialization
+    //input//   //weight0 = 0   //value//
+    //          //weight1 = 1   
+
+    // Use this for initialization
+    public VisualNet VNet;
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Layer[] layers = new Layer[2];
+        double[,] weights = new double[1, 2];
+        weights[0, 0] = 0;
+        weights[0, 1] = 1;
+        layers[0] = new Layer(1, 1, weights);
+        weights = new double[1, 2];
+        weights[0, 0] = 0;
+        weights[0, 1] = 1;
+        layers[1] = new Layer(1, 1, weights);
+		NeuralNet net = new NeuralNet(1, 1, 1, 1, layers);
+
+        VNet.net = net;
+        VNet.Initialize();
+
+        Debug.Log(net.FeedForward(new double[] { 0 })[0]);
+        Debug.Log(net.FeedForward(new double[] { 0.5 })[0]);
+        Debug.Log(net.FeedForward(new double[] { 1 })[0]);
+    }
+
 }
