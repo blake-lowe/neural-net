0265ab21d17b04166934772ac3f34ef8107e8791
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2019-03-19 13:09:24 -0400

    UI Input Fields Implemented

---
+++ Assets/Scripts/BoundTextField.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoundTextField : MonoBehaviour {
+
+    public InputField fieldToBound;
+    public bool boundedBelow;
+    public float min;
+    public bool boundedAbove;
+    public float max;
+
+    public void setValue()
+    {
+        float value = float.Parse(fieldToBound.text);
+        if (value > max && boundedAbove)
+        {
+            fieldToBound.text = max.ToString();
+        }
+        else if(value < min && boundedBelow)
+        {
+            fieldToBound.text = min.ToString();
+        }
+    }
+}
