6ac1674183619d2a74275be37d8642b578bb61e4
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2019-03-20 15:02:18 -0400

    Finished input fields and hooked into controller script

--- Assets/Scripts/CurveFit2DCtrl.cs
+++ Assets/Scripts/CurveFit2DCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CurveFit2DCtrl : MonoBehaviour {
 
@@ -32,6 +33,18 @@ public class CurveFit2DCtrl : MonoBehaviour {
     public float vNetYArea;
 
     public GameObject configPanel;
+    public InputField populationField;
+    public InputField numParentsField;
+    public InputField numCrossoverPointsField;
+    public InputField environmentalPressureField;
+    public InputField eliteFractionField;
+    public InputField tournamentSizeField;
+
+    public InputField numHiddenLayersField;
+    public InputField hiddenLayerSizeField;
+    public InputField numPointsField;
+    public InputField numTestPointsField;
+    public Dropdown functionDropdown;
 
     // Use this for initialization
     void Start()
@@ -68,7 +81,21 @@ public class CurveFit2DCtrl : MonoBehaviour {
     {
         if (function == "constant") { return 0.5f; }
         else if (function == "linear"){ return x; }
-        else if (function == "neglinear") { return 1f - x; }
+        else if (function == "negativeLinear") { return 1f - x; }
+        else if (function == "piecewiseLinear")
+        {
+            if(x >= 0 && x < 0.5f) { return 2f * x; }
+            else if(x >= 0.5f && x < 0.625f) { return 3f - 4f * x; }
+            else if(x >= 0.625f && x < 0.75f) { return -0.75f + 2f * x; }
+            else if(x >= 0.75f && x <= 1f) { return 2.25f - 2f * x; }
+            else { return 0f; }
+        }
+        else if (function == "quadratic") { return x*x; }
+        else if (function == "cubic") { return x*x*x; }
+        else if (function == "squareRoot") { return Mathf.Sqrt(x); }
+        else if (function == "exponential") { return Mathf.Exp(x); }
+        else if (function == "sine") { return Mathf.Sin(x); }
+        else if (function == "cosine") { return Mathf.Cos(x); }
         else { return 0f; }
     }
 
